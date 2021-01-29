0b541a510183cd4aedf6089b687496370e6845fb
Koichiro IWAO <meta@vmeta.jp>
2021-01-29 22:37:23 +0900

    apply rubocop

--- goodmoggoodnews.gemspec
+++ goodmoggoodnews.gemspec
@@ -11,13 +11,13 @@ Gem::Specification.new do |spec|
   spec.summary       = "もぐもぐアラート"
   spec.description   = "the pillowsファンクラブのブログ更新をお知らせします "
   spec.homepage      = "https://github.com/metalefty/goodmoggoodnews"
-  spec.required_ruby_version = Gem::Requirement.new(">= 2.3.0")
+  spec.required_ruby_version = Gem::Requirement.new(">= 2.6.0")
 
-  #spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  # spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = "https://github.com/metalefty/goodmoggoodnews"
-  #spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  # spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
--- lib/goodmoggoodnews.rb
+++ lib/goodmoggoodnews.rb
@@ -2,18 +2,18 @@
 
 require_relative "goodmoggoodnews/version"
 
-require 'twitter'
-require 'faraday'
-require 'faraday_middleware'
-require 'nokogiri'
-require 'active_support'
-require 'active_support/core_ext'
+require "twitter"
+require "faraday"
+require "faraday_middleware"
+require "nokogiri"
+require "active_support"
+require "active_support/core_ext"
 
 class Goodmoggoodnews
-  BLOG_URL_PREFIX = 'https://fan.pia.jp/the-pillows/news/detail/'
-  PHOTOS_URL_PREFIX = 'https://fan.pia.jp/the-pillows/photo/detail/'
+  BLOG_URL_PREFIX = "https://fan.pia.jp/the-pillows/news/detail/"
+  PHOTOS_URL_PREFIX = "https://fan.pia.jp/the-pillows/photo/detail/"
   USER_AGENT = "GoodMogGoodNews/#{Goodmoggoodnews::VERSION}; https://twitter.com/GoodMogGoodNews"
-  #USER_AGENT = "DanceWithBot/#{Goodmoggoodnews::VERSION}; https://twitter.com/DanceWithBot"
+  # USER_AGENT = "DanceWithBot/#{Goodmoggoodnews::VERSION}; https://twitter.com/DanceWithBot"
   class Error < StandardError; end
 
   # Twitter関連の操作
@@ -22,18 +22,18 @@ class Goodmoggoodnews
 
     def initialize
       @client = ::Twitter::REST::Client.new(
-        consumer_key:         ENV['CONSUMER_KEY'],
-        consumer_secret:      ENV['CONSUMER_SECRET'],
-        access_token:         ENV['ACCESS_TOKEN'],
-        access_token_secret:  ENV['ACCESS_TOKEN_SECRET'],
+        consumer_key: ENV["CONSUMER_KEY"],
+        consumer_secret: ENV["CONSUMER_SECRET"],
+        access_token: ENV["ACCESS_TOKEN"],
+        access_token_secret: ENV["ACCESS_TOKEN_SECRET"]
       )
 
       # TwitterプロフィールのLocationを最新記事の番号の保存場所として使う
-      @location =  {}
+      @location = {}
       begin
         json = @client&.user&.location
         @location = JSON.parse(json, symbolize_names: true)
-      rescue JSON::ParserError => e
+      rescue JSON::ParserError
         @location = { n: 0, p: 0 }
       end
     end
@@ -64,7 +64,7 @@ class Goodmoggoodnews
 
       title = html.xpath('/html/head/meta[@property="og:title"]/@content').text
       title = "タイトル取得失敗" if title.blank?
-      date = html.css('article.newsArticle').css('p.date').text
+      date = html.css("article.newsArticle").css("p.date").text
       date = "日付取得失敗" if date.blank?
 
       "#{date} #{title}"
@@ -72,18 +72,16 @@ class Goodmoggoodnews
 
     def self.scrape_photo_page(string)
       html = Nokogiri::HTML.parse(string)
-      
+
       title = html.xpath('/html/head/meta[@property="og:title"]/@content').text
       title = "タイトル取得失敗" if title.blank?
 
       "【PHOTO】#{title}"
     end
-
   end
 
   # 最新記事をチェックするためのモジュール
   module Crawler
-
     def self.crawl(id: 0, type: nil)
       prefix = ""
 
@@ -98,14 +96,14 @@ class Goodmoggoodnews
 
       retval = []
 
-      (id+1..).each do |i|
-        u = URI.parse("#{prefix}")
+      (id + 1..).each do |i|
+        u = URI.parse(prefix.to_s)
 
         conn = Faraday::Connection.new(u,
-          headers:
-          {
-            "User-Agent": USER_AGENT
-          }) do |builder|
+                                       headers:
+                                       {
+                                         "User-Agent": USER_AGENT
+                                       }) do |builder|
           builder.use Faraday::Response::Logger
           builder.use FaradayMiddleware::FollowRedirects
         end
@@ -117,6 +115,7 @@ class Goodmoggoodnews
         Goodmoggoodnews.sleep_random
 
         break unless response.success?
+
         retval << { id: i, response: response }
       end
 
@@ -126,21 +125,21 @@ class Goodmoggoodnews
     # 指定されたURIの記事を取得
     def self.get(uri)
       conn = Faraday::Connection.new(URI(uri),
-        headers:
-        {
-          "User-Agent": USER_AGENT
-        }) do |builder|
+                                     headers:
+                                     {
+                                       "User-Agent": USER_AGENT
+                                     }) do |builder|
         builder.use Faraday::Response::Logger
         builder.use FaradayMiddleware::FollowRedirects
       end
 
       response = conn.get
+
+      response
     end
   end
 
-  private
-
-  def self.sleep_random(i = 10)
-    sleep SecureRandom.random_number(i)
+  def self.sleep_random(wait = 10)
+    sleep SecureRandom.random_number(wait)
   end
 end
